a4ee700bd7c6fc33495ae59767d6e7bb29eae694
Hayden Lester <haydenlester99@protonmail.com>
2024-03-29 22:51:54 -0500

    refac: renamed module

--- src/HaskList/Domain/Task.hs
+++ src/HaskList/Domain/Task.hs
@@ -5,14 +5,6 @@ import Data.Time (UTCTime)
 import System.Cron.Types
 
 ---------------------------------------------------------------
-data Priority = High | Medium | Low
-  deriving (Show, Eq, Ord)
-
-data Schedule = Schedule
-  { next        :: Maybe CronSchedule
-  , due         :: UTCTime
-  } deriving (Show, Eq)
-
 data Task = Task
   { index       :: Int
   , priority    :: Priority
@@ -20,14 +12,6 @@ data Task = Task
   , completion  :: (Int, Int)
   , schedule    :: Schedule
   } deriving (Show, Eq)
-
-
-instance Ord Task where
-  compare t1 t2 = compare (index' t1) (index' t2)
-
----------------------------------------------------------------
-
--- Accessors
 
 index'      :: Task -> Int
 priority'   :: Task -> Priority
@@ -40,3 +24,23 @@ priority' (Task{priority = ret}) = ret
 desc' (Task{desc = ret}) = ret
 completion' (Task{completion = ret}) = ret
 schedule' (Task{schedule = ret}) = ret
+
+---------------------------------------------------------------
+
+data Schedule = Schedule
+  { next        :: Maybe CronSchedule
+  , due         :: UTCTime
+  } deriving (Show, Eq)
+
+next' :: Schedule -> Maybe CronSchedule
+due'  :: Schedule -> UTCTime
+
+next' (Schedule{next = ret}) = ret
+due' (Schedule{due = ret}) = ret
+
+---------------------------------------------------------------
+
+data Priority = High | Medium | Low
+  deriving (Show, Eq, Ord)
+
+---------------------------------------------------------------
