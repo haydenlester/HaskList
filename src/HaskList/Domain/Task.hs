b3213ddaa5dd9835b1bd71c50f059b8d2e182def
Hayden Lester <haydenlester99@protonmail.com>
2024-05-30 18:28:32 -0500

    refac: fix some stupidity

--- src/HaskList/Domain/Task.hs
+++ src/HaskList/Domain/Task.hs
@@ -1,39 +1,21 @@
+-- Represents the tasks that the user
+-- creates.
+
 module HaskList.Domain.Task where
 
-import Data.Time (UTCTime)
+import Data.Time (TimeOfDay)
 
----------------------------------------------------------------
 data Task = Task
   { index       :: Int
   , priority    :: Priority
   , desc        :: String
   , completion  :: (Int, Int)
-  , due         :: UTCTime
   , schedule    :: Maybe Schedule
-  } deriving (Show, Eq)
-
-index'      :: Task -> Int
-priority'   :: Task -> Priority
-desc'       :: Task -> String
-completion' :: Task -> (Int, Int)
-due' :: Task -> UTCTime
-schedule'   :: Task -> Maybe Schedule
-
-index' (Task{index = ret}) = ret
-priority' (Task{priority = ret}) = ret
-desc' (Task{desc = ret}) = ret
-completion' (Task{completion = ret}) = ret
-due' (Task{due = ret}) = ret
-schedule' (Task{schedule = ret}) = ret
-
----------------------------------------------------------------
+  , due         :: TimeOfDay
+  } deriving (Show, Read, Eq)
 
 data Priority = High | Medium | Low
-  deriving (Show, Eq, Ord)
+  deriving (Show, Read, Eq)
 
----------------------------------------------------------------
-
-data Schedule = Daily | Weekly | Monthly | Yearly
-  deriving (Show, Eq)
-
----------------------------------------------------------------
+data Schedule = Daily | Weekly | Monthly
+  deriving (Show, Read, Eq)
