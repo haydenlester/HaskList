983b14712ecf05fa88be29c306294c571587e0f8
Hayden Lester <haydenlester99@protonmail.com>
2024-05-31 20:28:21 -0500

    feat: added dl guarded read

---
+++ src/HaskList/Data/TaskRepository.hs
@@ -0,0 +1,34 @@
+module HaskList.Data.TaskRepository where
+
+import System.Directory
+import System.IO.Error
+import Control.Exception
+import Control.Monad
+
+import HaskList.Domain.Task
+
+type Strategy = Either () String -> String
+
+-- Relative path to our cache file.
+-- May want to lift to a config file.
+rPath :: FilePath
+rPath = "/.hasklist/hasklist.cache"
+
+-- Retrieves the home directory for the user
+-- and appends the relative path.
+fqPath :: IO FilePath
+fqPath = fmap (++ rPath) getHomeDirectory
+
+-- Strategy for resolving ()
+-- Reads an empty list in.
+dummyStrategy :: Strategy
+dummyStrategy = either (const "[]") id
+
+-- Guards for cases where the file may not
+-- exist. Applies a strategy to resolve it
+guardRead :: IO String -> Strategy -> IO String
+guardRead = (flip fmap) . tryJust (guard . isDoesNotExistError)
+
+-- Parse the tasks
+readTasks :: IO [Task]
+readTasks = fmap read $ guardRead (fqPath >>= readFile) dummyStrategy
